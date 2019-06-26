33b119c6b5924db0a729defc41c73db8ef1b87bb
kants3 <shashi.kant@in.pega.com>
2019-06-26 14:03:33 +0500

    tst

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,4 +4,4 @@ set -e
 
 curl -LO https://storage.googleapis.com/container-structure-test/latest/container-structure-test-linux-amd64 && chmod +x container-structure-test-linux-amd64 && sudo mv container-structure-test-linux-amd64 /usr/local/bin/container-structure-test
 
-container-structure-test test --image myimage --config src/test/container-structure-test/jdk_testcases.yaml
+container-structure-test test --image myimage --config scripts/jdk_testcases.yaml
